3d5c18def2e74cc5a133cec06448e904acd8a32e
JericLew <lew.jeric@gmail.com>
2022-02-21 22:18:45 +0800

    addded Centering and appraoch todo: 1. implement front_distance calculation 2. implement shotting sequence 3. actually test it 4. create script for interfacing and publishing thermal array

--- r2targeting1.py
+++ r2targeting1.py
@@ -2,16 +2,19 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from rclpy.qos import qos_profile_sensor_data
+from std_msgs.msg import Float32MultiArray, Float64MultiArray
+import numpy as np
 import time
 
 ''' Subcribe to Thermal Cam Data (array) and publish to twist topic for rotation to centralise
 and forward'''
 
 #variable
-movespeed = 0.05
-turnspeed = 0.1
+speedchange = 0.05
+rotatechange = 0.1
 hot_threshold = 30
-shoot_threshold = 35
+shoot_distance = 30
+
 
 
 class Targeter(Node):
@@ -24,25 +27,102 @@ class Targeter(Node):
 
         #create subscription to thermal cam array data
         self.subscription = self.create_subscription(
-            String,
+            Float64MultiArray,
             'thermal',
             self.thermal_callback,
             10)
         self.subscription  # prevent unused variable warning
 
-    def thermal_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
-        
+        #some data
+        self.thermal_array = []
+        self.target_presence = False
+        self.front_distance = -1
+
+#function to  store array data in self.thermal_array
+    def thermal_callback(self, thermal_array):
+        self.get_logger().info('I heard: "%s"' % thermal_array.data)
+        self.thermal_array = thermal_array.data
+
+#function to stop bot
     def stopbot(self):
         twist = Twist()
         twist.linear.x = 0.0
         twist.angular.z = 0.0
-        self.publisher_.publish(twist)    
+        self.publisher_.publish(twist) 
 
+#funtion that moves bot forward by speedchange variable
+    def robotforward(self):
+        # start moving
+        self.get_logger().info('Start moving')
+        twist = Twist()
+        twist.linear.x = speedchange
+        twist.angular.z = 0.0
+        # not sure if this is really necessary, but things seem to work more
+        # reliably with this
+        self.publisher_.publish(twist)
+        twist.linear.x = 0.0
+        time.sleep(1.5)
+        self.publisher_.publish(twist)
+
+
+#function that checks through array if there is anything hot
+    def detect_target(self):
+        while self.target_presence == False:
+            for row in range(len(self.thermal_array)):
+                for col in range(len(self.thermal_array[row])):
+                    if self.thermal_array[row][col] >= hot_threshold:
+                        self.target_presence == True
+                    else:
+                        self.target_presence == False
+
+    def center_target(self):
+        max_row = 0
+        max_col = 0
+        max_value = 0.0
+        view = self.thermal_array
+        while max_col >4 or max_col<3:
+            for row in range(len(self.thermal_array)):
+                for col in range(len(self.thermal_array[row])):
+                    current_value = view[row][col]
+                    if current_value > max_value:
+                        max_row = row
+                        max_col = col
+                        max_value = current_value
+            if max_col < 3:
+                    # spin it anti-clockwise
+                    twist = Twist()
+                    twist.linear.x = 0.0
+                    twist.angular.z = rotatechange
+                    time.sleep(1)
+                    self.publisher_.publish(twist)
+                    time.sleep(1)
+            elif max_col > 4:
+                    # spin it clockwise
+                    twist = Twist()
+                    twist.linear.x = 0.0
+                    twist.angular.z = -1 * rotatechange
+                    time.sleep(1)
+                    self.publisher_.publish(twist)
+                    time.sleep(1)
+            else:
+                self.stopbot
+                    
 def main(args=None):
     rclpy.init(args=args)
 
     targeter = Targeter()
+    #input targeter node functions
+
+    #trying to detect hot target. if detected, stop
+    while targeter.target_presence == False:
+        targeter.detect_target()
+        if targeter.target_presence == True:
+            targeter.stopbot()
+    while targeter.front_distance < shoot_distance:
+        targeter.center_target()
+        targeter.robotforward
+    
+    
     
 
     targeter.destroy_node()
