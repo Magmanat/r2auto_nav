473af8fdd0c1d824e4cbd65fbe2c44898fc82574
Magmanat <princemjp@gmail.com>
2022-03-06 05:00:42 +0800

    added full round wall following and cutthrough algo

--- r2_wall_follow.py
+++ r2_wall_follow.py
@@ -34,7 +34,8 @@ rotatechange = 0.1
 speedchange = 0.05
 occ_bins = [-1, 0, 50, 101]
 stop_distance = 0.25
-front_angle = 30
+initialdelay = 5
+front_angle = 20
 front_angles = range(-front_angle,front_angle+1,1)
 
 
@@ -82,11 +83,11 @@ class AutoNav(Node):
         self.yaw = 0
         
         # create subscription to track occupancy
-        # self.occ_subscription = self.create_subscription(
-        #     OccupancyGrid,
-        #     'map',
-        #     self.occ_callback,
-        #     1)
+        self.occ_subscription = self.create_subscription(
+            OccupancyGrid,
+            'map',
+            self.occ_callback,
+            1)
         # self.occ_subscription  # prevent unused variable warning
         self.occdata = np.array([])
         
@@ -103,12 +104,23 @@ class AutoNav(Node):
         'NFC_presence',
         self.NFC_callback,
         10)
+        self.buttonsubscription = self.create_subscription(Bool,
+        'button_presence',
+        self.button_callback,
+        10)
         self.resolution = 0.05
         self.nfc_presence = False
+        self.button_presence = False
+        self.loaded = False
+        self.recordedinitial = False
+        self.moved_off = False
+        self.one_round = False
         self.Xpos = 0
         self.Ypos = 0
         self.XposNoAdjust = 0
         self.YposNoAdjust = 0
+        self.Xstart = 0
+        self.Ystart = 0
         self.mapbase = 0
         self.mazelayout = []
 
@@ -118,6 +130,12 @@ class AutoNav(Node):
         else:
             self.nfc_presence = False
 
+    def button_callback(self,msg):
+        if msg.data == True:
+            self.button_presence = True
+        else:
+            self.button_presence = False
+
     def map2base_callback(self, msg):
         # self.get_logger().info('In map2basecallback')
         
@@ -125,35 +143,35 @@ class AutoNav(Node):
         self.roll, self.pitch, self.yaw = euler_from_quaternion(msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
 
 
-    # def occ_callback(self, msg):
-    #     # self.get_logger().info('In occ_callback')
-    #     occdata = np.array(msg.data)
-    #     # compute histogram to identify bins with -1, values between 0 and below 50, 
-    #     # and values between 50 and 50. The binned_statistic function will also
-    #     # return the bin numbers so we can use that easily to create the image 
-    #     occ_counts, edges, binnum = scipy.stats.binned_statistic(occdata, np.nan, statistic='count', bins=occ_bins)
-    #     # get width and height of map
-    #     iwidth = msg.info.width
-    #     iheight = msg.info.height
-    #     # calculate total number of bins
-    #     total_bins = iwidth * iheight
-    #     # log the info
-    #     # self.get_logger().info('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i' % (occ_counts[0], occ_counts[1], occ_counts[2], total_bins))
+    def occ_callback(self, msg):
+        # self.get_logger().info('In occ_callback')
+        occdata = np.array(msg.data)
+        # compute histogram to identify bins with -1, values between 0 and below 50, 
+        # and values between 50 and 50. The binned_statistic function will also
+        # return the bin numbers so we can use that easily to create the image 
+        occ_counts, edges, binnum = scipy.stats.binned_statistic(occdata, np.nan, statistic='count', bins=occ_bins)
+        # get width and height of map
+        iwidth = msg.info.width
+        iheight = msg.info.height
+        # calculate total number of bins
+        total_bins = iwidth * iheight
+        # log the info
+        # self.get_logger().info('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i' % (occ_counts[0], occ_counts[1], occ_counts[2], total_bins))
 
-    #     # binnum go from 1 to 3 so we can use uint8
-    #     # convert into 2D array using column order
-    #     self.mazelayout = np.uint8(binnum.reshape(msg.info.height,msg.info.width))
-
-    #     self.Xpos = int(np.rint((self.mapbase.x - msg.info.origin.position.x)/self.resolution))
-    #     self.Ypos = int(np.rint((self.mapbase.y - msg.info.origin.position.y)/self.resolution))
-    #     self.mazelayout[self.Ypos][self.Xpos] = 6
-    #     self.Xadjust = msg.info.origin.position.x
-    #     self.Yadjust = msg.info.origin.position.y
-    #     img2 = Image.fromarray(self.mazelayout)
-    #     plt.imshow(img2, cmap='gray', origin='lower')
-    #     plt.draw_all()
-    #     # # pause to make sure the plot gets created
-    #     plt.pause(0.00000000001)
+        # binnum go from 1 to 3 so we can use uint8
+        # convert into 2D array using column order
+        self.mazelayout = np.uint8(binnum.reshape(msg.info.height,msg.info.width))
+        self.Xadjust = msg.info.origin.position.x
+        self.Yadjust = msg.info.origin.position.y
+        self.Xpos = self.mapbase.x
+        self.Ypos = self.mapbase.y
+        # self.mazelayout[self.Ypos][self.Xpos] = 6
+        
+        # img2 = Image.fromarray(self.mazelayout)
+        # plt.imshow(img2, cmap='gray', origin='lower')
+        # plt.draw_all()
+        # # # pause to make sure the plot gets created
+        # plt.pause(0.00000000001)
 
 
     def scan_callback(self, msg):
@@ -180,7 +198,7 @@ class AutoNav(Node):
         # 360 to 0, or from -180 to 180
         c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
         # calculate desired yaw
-        target_yaw = math.radians(rot_angle)
+        target_yaw = current_yaw + math.radians(rot_angle)
         # convert to complex notation
         c_target_yaw = complex(math.cos(target_yaw),math.sin(target_yaw))
         self.get_logger().info('Desired: %f' % math.degrees(cmath.phase(c_target_yaw)))
@@ -220,7 +238,7 @@ class AutoNav(Node):
         self.publisher_.publish(twist)
 
     def pick_direction(self):
-        self.get_logger().info('In pick direction:')
+        # self.get_logger().info('In pick direction:')
         
         self.front_dist = np.nan_to_num(
             self.laser_range[0], copy=False, nan=100)
@@ -229,9 +247,9 @@ class AutoNav(Node):
         self.rightfront_dist = np.nan_to_num(
             self.laser_range[315], copy=False, nan=100)
 
-        self.get_logger().info('Front Distance: %s' % str(self.front_dist))
-        self.get_logger().info('Front Left Distance: %s' % str(self.leftfront_dist))
-        self.get_logger().info('Front Right Distance: %s' % str(self.rightfront_dist))
+        # self.get_logger().info('Front Distance: %s' % str(self.front_dist))
+        # self.get_logger().info('Front Left Distance: %s' % str(self.leftfront_dist))
+        # self.get_logger().info('Front Right Distance: %s' % str(self.rightfront_dist))
 
         # Logic for following the wall
         # >d means no wall detected by that laser beam
@@ -240,8 +258,8 @@ class AutoNav(Node):
         # Set turning speeds (to the left) in rad/s
 
         # These values were determined by trial and error.
-        self.turning_speed_wf_fast = 0.75  # Fast turn ideal = 1.0
-        self.turning_speed_wf_slow = 0.40  # Slow turn = 0.50
+        self.turning_speed_wf_fast = 0.60  # Fast turn ideal = 1.0
+        self.turning_speed_wf_slow = 0.2  # Slow turn = 0.4
         # Set movement speed
         self.forward_speed = speedchange
         # Set up twist message as msg
@@ -311,6 +329,173 @@ class AutoNav(Node):
         # time.sleep(1)
         self.publisher_.publish(twist)
 
+    def loading(self):
+        self.stopbot()
+        while self.button_presence == False:
+            rclpy.spin_once(self)
+        self.loaded = True
+        
+    def recordposition(self):
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("######################################")
+        print("RECORDED POSITION")
+        self.Xstart = self.Xpos
+        self.Ystart = self.Ypos
+
+
+    def move_forward(self):
+        # start moving forward
+        self.get_logger().info('Moving forward')
+        twist = Twist()
+        twist.linear.x = speedchange
+        twist.angular.z = 0.0
+        # not sure if this is really necessary, but things seem to work more
+        # reliably with this
+        time.sleep(0.2)
+        self.publisher_.publish(twist)
+
+    def get_side_wall_distance(self, direction):
+      if self.laser_range[direction-5:direction+5] == []:
+        return 5
+      return np.average(self.laser_range[direction-5:direction+5])
+      
+    def cut_through(self):
+        startingX = ((len(self.mazelayout[0]) * self.resolution) / 2)
+        startingY = 0
+        print(self.Xadjust) 
+        while abs(self.Xpos - startingX) > 0.25 or  abs(self.Ypos - startingY) > 0.25:
+            self.pick_direction()
+            print("in cut through")
+            rclpy.spin_once(self)
+            print("target")
+            print(startingX)
+            print(startingY)
+            print("current")
+            print(self.Xpos)
+            print(self.Ypos)
+        self.rotatebot(-90)
+        self.move_forward()
+        initialY = self.Ypos
+        # check prev u-turn direction
+        while True:
+            self.move_forward()
+            while True:
+                rclpy.spin_once(self)
+                lri = (self.laser_range[front_angles]<float(stop_distance)).nonzero()
+                            # self.get_logger().info('Distances: %s' % str(lri))
+                            # check if the initial position has been set
+
+                            # if the list is not empty
+                if(len(lri[0])>0):
+                    # stop moving
+                    self.stopbot()
+                    self.get_logger().info('Obstacle encountered infront')
+                    break
+            print("Y initial and difference")
+            print(initialY)
+            print(abs(initialY - self.Ypos))
+            print(len(self.mazelayout) * self.resolution - 2.0)
+            if (abs(initialY - self.Ypos) >= len(self.mazelayout) * self.resolution - 2.0):
+                break
+            bypass_direction = -90
+            bypass_opp_dir = 90
+
+            # variable used to hold the current x and y position of the turtlebot
+            initialX = self.Xpos
+            self.get_logger().info('Initial x position is: %.2f' % initialX)
+            # turn the turtlebot before starting adopted Pledge algorithm
+            self.get_logger().info('Turtlebot turned %s to start Pledge Algo' % bypass_direction)
+            self.rotatebot(bypass_direction)
+            # start moving forward after turn is made
+            self.move_forward()
+            # setting the first avg wall distance
+            prev_wall_avg_side_distance = self.get_side_wall_distance(bypass_opp_dir)
+            # variable to prevent turtlebot from exiting function even before it starts moving
+            moved_off = False
+
+            # Loop to continuously check till turtlebot is done
+            # Breaks out of loop automatically when done
+            while (True):
+
+                # to update the laser_range values
+                rclpy.spin_once(self)
+                print("initial")
+                print(initialX)
+                print("current")
+                print(self.Xpos)
+                # check if the turtlebot has reached the other side after moving off, with an allowance of 1 cm
+                if (moved_off and (abs(initialX - self.Xpos) <= 0.15)):
+                    # turtlebot has successfully navigated around the obstacle, exiting function to resume normal navigation
+                    self.get_logger().info('Turtlebot successfully navigated around the obstacle')
+                    self.rotatebot(bypass_direction)
+                    break
+                    self.move_forward()
+
+                # check if there is obstacle in front blocking the way
+                if self.laser_range.size != 0:
+                    # check distances in front of TurtleBot and find values less
+                    # than stop_distance
+                    front_wall = (self.laser_range[front_angles]<float(stop_distance)).nonzero()
+
+                    if(len(front_wall[0])>0):
+                        # stop moving
+                        self.stopbot()
+                        self.get_logger().info('Obstacle encountered infront')
+
+                        # exit bypass function if edge of maze is reached
+                        if (abs(initialY - self.Ypos) >= len(self.mazelayout) * self.resolution - 2):
+                            self.get_logger().info('Reached the edge of the maze while bypassing obs')
+                            return
+                        if np.take(self.laser_range, 90) > np.take(self.laser_range, 270):
+                            self.rotatebot(90)
+                        else:
+                            self.rotatebot(-90)
+
+                        self.move_forward()
+
+                # calculate the current avg distance from wall from front and side
+                # self.get_logger().info('Updating current average wall distance')
+                curr_wall_avg_side_distance = self.get_side_wall_distance(bypass_opp_dir)
+                if (curr_wall_avg_side_distance < prev_wall_avg_side_distance):
+                        # reset the the average distance of the wall on the wall
+                        prev_wall_avg_side_distance = curr_wall_avg_side_distance
+                        self.get_logger().info('New previous average wall distance is %.2f' % prev_wall_avg_side_distance)
+                # check if the obstacle is still on the side
+                # if distance suddenly increases significantly, obstacle no longer on the side
+                # checked by if the avg distance between previous and current distance from wall defer by more than 50%
+                # and the prev_wall_avg_distance needs to be less than 0.5m away to ensure the wall has been detected before
+                distance_diff = curr_wall_avg_side_distance - prev_wall_avg_side_distance
+                if ((distance_diff > 2 * prev_wall_avg_side_distance) and (prev_wall_avg_side_distance <= 0.5)):
+                    self.get_logger().info('Side wall no longer detected')
+                    self.get_logger().info('Distance diff is this %.2f' % distance_diff)
+                    # extra distance so that the turtlebot has sufficient space to turn
+                    timenow = time.time()
+                    while time.time() - timenow < 0.8:
+                        print("stopping")
+                    self.stopbot()
+                    # wall no longer detected, stop and rotate turtlebot   
+                    self.rotatebot(bypass_opp_dir)
+                    # set moved_off to True only once
+                    if (moved_off == False):
+                        moved_off = True
+                        self.get_logger().info('Moved_off set to True')
+
+                    # reset the avg side wall distance
+                    self.get_logger().info('Resetting prev average side wall distance')
+                    prev_wall_avg_side_distance = self.get_side_wall_distance(bypass_opp_dir)
+                    self.get_logger().info('New prev average wall distance is %.2f' % prev_wall_avg_side_distance)
+                    self.move_forward()
+        
+
+        
 
     def mover(self):
         try:
@@ -320,8 +505,42 @@ class AutoNav(Node):
             while (self.laser_range.size == 0):
                 print("Spin to get a valid lidar data")
                 rclpy.spin_once(self)
-              
+            self.timenow = time.time()
             while rclpy.ok():
+                # print(time.time())
+                print("target")
+                print(self.Xstart)
+                print(self.Ystart)
+                print("current")
+                print(self.Xpos)
+                print(self.Ypos)
+                if time.time() - self.timenow > initialdelay and self.recordedinitial == False:
+                    self.recordedinitial = True
+                    self.recordposition()
+                if self.moved_off == False and (abs(self.Xstart - self.Xpos) > 0.25 or abs(self.Ystart - self.Ypos) > 0.25) and self.recordedinitial:
+                    print("################")
+                    print("################")
+                    print("################")
+                    print("################")
+                    print("################")
+                    print("moved off")
+                    self.moved_off = True
+                if self.moved_off and self.loaded and (abs(self.Xstart - self.Xpos) < 0.5 and abs(self.Ystart - self.Ypos) < 0.5) and self.one_round == False:
+                    print("################")
+                    print("################")
+                    print("################")
+                    print("################")
+                    print("################")
+                    print("moved one round")
+                    self.one_round = True
+
+                if self.loaded == False and self.nfc_presence == True:
+                  self.loading()
+                if self.loaded and self.one_round:
+                  self.cut_through()
+                  self.stopbot()
+                  print("IM DONE##################################################################################")
+                  break
                 self.pick_direction()
                 # allow the callback functions to run
                 rclpy.spin_once(self)
@@ -349,6 +568,7 @@ def main(args=None):
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
     auto_nav.destroy_node()
+    #do some thermal nav node and shooting node here
     rclpy.shutdown()
 
 
