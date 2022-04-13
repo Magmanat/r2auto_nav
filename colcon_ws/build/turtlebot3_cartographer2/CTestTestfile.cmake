77f92fbcae8c30938f403dca7eb5a2b155823ca2
Magmanat <princemjp@gmail.com>
2022-04-13 16:33:18 +0800

    removed build and install

--- colcon_ws/build/turtlebot3_cartographer2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prince/colcon_ws/src/turtlebot3_simulations/turtlebot3_cartographer2
-# Build directory: /home/prince/colcon_ws/build/turtlebot3_cartographer2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
